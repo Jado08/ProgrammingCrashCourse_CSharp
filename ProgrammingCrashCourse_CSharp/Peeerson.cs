ee1ffbf87d0191f6432b745e0ee7563e518d1399
Jado08 <julieanndingalorcine@gmail.com>
2024-09-18 10:45:09 +0800

    third commit (OOP-Classes and Objects)

---
+++ ProgrammingCrashCourse_CSharp/Peeerson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCrashCourse_CSharp
+{
+    internal class Peeerson
+    {
+       public string firstName;
+       public string lastName;
+       public int age;
+       public char sex;
+    }
+}
--- ProgrammingCrashCourse_CSharp/Program.cs
+++ ProgrammingCrashCourse_CSharp/Program.cs
@@ -10,19 +10,76 @@ namespace ProgrammingCrashCourse_CSharp
     {
         static void Main(string[] args)
         {
-            //ProgrammingCrashCourse.Helloworld();
-            //ProgrammingCrashCourse.DataTypes();
-            //ProgrammingCrashCourse.BasicIO();
-            //ProgrammingCrashCourse.Arithmetic();
-            //ProgrammingCrashCourse.ConditionalStatements();
+            int choice = 0; 
+            bool isValid = false;
 
-            //ProgrammingCrashCourse.Switch();
-            //ProgrammingCrashCourse.WhileLoop();
-            //ProgrammingCrashCourse.DoWhileLoop();
-            //ProgrammingCrashCourse.ForLoop();
+            while (!isValid)
+            {
+                Console.WriteLine("Choose an option: ");
+                Console.WriteLine("1. Hello World");
+                Console.WriteLine("2. Variables and Data Types");
+                Console.WriteLine("3. Basic Input and Output");
+                Console.WriteLine("4. Operators and Expressions");
+                Console.WriteLine("5. Conditional Statements");
+                Console.WriteLine("6. Switch Statements");
+                Console.WriteLine("7. While Loop");
+                Console.WriteLine("8. Do While Loop");
+                Console.WriteLine("9. For Loop");
+                Console.WriteLine("10. For Each Loop");
+                Console.WriteLine("11. Arrays");
+                Console.WriteLine("12. Classes & Objects");
 
-            //ProgrammingTutorials.ForEachLoop();
-            ProgrammingTutorials.Arrays();
+                Console.Write("\nYour Option No. ");
+
+                isValid = int.TryParse(Console.ReadLine(), out choice);
+
+                if (!isValid)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.\n");
+                }
+            }
+            switch (choice)
+            {
+                case 1: ProgrammingCrashCourse.Helloworld(); break;
+                case 2: ProgrammingCrashCourse.DataTypes(); break;
+                case 3: ProgrammingCrashCourse.BasicIO(); break;
+                case 4: ProgrammingCrashCourse.Arithmetic(); break;
+                case 5: ProgrammingCrashCourse.ConditionalStatements(); break;
+                case 6: ProgrammingCrashCourse.Switch(); break;
+                case 7: ProgrammingCrashCourse.WhileLoop(); break;
+                case 8: ProgrammingCrashCourse.DoWhileLoop(); break;
+                case 9: ProgrammingCrashCourse.ForLoop(); break;
+                case 10: ProgrammingTutorials.ForEachLoop(); break;
+                case 11: ProgrammingTutorials.Arrays(); break;
+                case 12:
+                    Peeerson p1 = new Peeerson();
+                    p1.firstName = "Julie";
+                    p1.lastName = "Orcine";
+                    p1.age = 20;
+                    p1.sex = 'F';
+
+                    Peeerson p2 = new Peeerson();
+                    p2.firstName = "Daniel";
+                    p2.lastName = "Orcine";
+                    p2.age = 19;
+                    p2.sex = 'M';
+
+                    Peeerson p3 = new Peeerson();
+                    p3.firstName = "Jeneth";
+                    p3.lastName = "Orcine";
+                    p3.age = 19;
+                    p3.sex = 'F';
+
+                    Console.WriteLine("\n" + p1.firstName + " " + p1.lastName + " " +
+                        p1.age + " " + p1.sex);
+                    Console.WriteLine(p2.firstName + " " + p2.lastName + " " +
+                        p2.age + " " + p2.sex);
+                    Console.WriteLine(p3.firstName + " " + p3.lastName + " " +
+                        p3.age + " " + p3.sex);
+                    break;
+
+                default: Console.WriteLine("Invalid Choice"); break;
+            }
         }
     }
 }
